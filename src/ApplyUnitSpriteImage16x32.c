98af51c60a552a6849d33b11e99eacd3cb18e427
laqieer <laqieer@126.com>
2026-06-17 18:32:35 +0000

    feat(handdecomp): coddog-carve ApplyUnitSpriteImage16x32

---
+++ src/ApplyUnitSpriteImage16x32.c
@@ -0,0 +1,40 @@
+#include "global.h"
+#include "hardware.h"
+#include "unit_icon_data.h"
+#include "bmunit.h"
+#include "bmmap.h"
+#include "bmtrick.h"
+#include "chapterdata.h"
+#include "ctc.h"
+#include "mu.h"
+#include "worldmap.h"
+#include "bmudisp.h"
+#include "bmlib.h"
+#include "constants/terrains.h"
+#include "constants/event-flags.h"
+#include "constants/video-global.h"
+
+
+
+extern u16  sSlotToChrLut[];
+extern u8*  UnitSpriteUnpackBuf;
+extern u8  gSMSGfxBuffer[3][8*0x20*0x20];
+
+int ApplyUnitSpriteImage16x32(int slot, u32 id)
+{
+    int i;
+
+    int outOff = sSlotToChrLut[slot] * CHR_SIZE;
+    id = ((id >> UNITSPRITE_ID_BITS) ^ 1) & 1;
+
+    for (i = 0; i < 3; i++)
+    {
+        int imgOff = (i * id) * 8 * CHR_SIZE;
+
+        CpuFastCopy(UnitSpriteUnpackBuf + 0 * CHR_SIZE + imgOff, gSMSGfxBuffer[i] + 0 * CHR_SIZE * CHR_LINE + outOff, 2 * CHR_SIZE);
+        CpuFastCopy(UnitSpriteUnpackBuf + 2 * CHR_SIZE + imgOff, gSMSGfxBuffer[i] + 1 * CHR_SIZE * CHR_LINE + outOff, 2 * CHR_SIZE);
+        CpuFastCopy(UnitSpriteUnpackBuf + 4 * CHR_SIZE + imgOff, gSMSGfxBuffer[i] + 2 * CHR_SIZE * CHR_LINE + outOff, 2 * CHR_SIZE);
+        CpuFastCopy(UnitSpriteUnpackBuf + 6 * CHR_SIZE + imgOff, gSMSGfxBuffer[i] + 3 * CHR_SIZE * CHR_LINE + outOff, 2 * CHR_SIZE);
+    }
+    return sSlotToChrLut[slot];
+}
