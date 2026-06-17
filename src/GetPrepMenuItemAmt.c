e04059de0edd28b30b3b641cb91b7d880fcfbba3
laqieer <laqieer@126.com>
2026-06-17 14:02:59 +0000

    feat(handdecomp): coddog-carve GetPrepMenuItemAmt (auto straight)

---
+++ src/GetPrepMenuItemAmt.c
@@ -0,0 +1,35 @@
+#include "global.h"
+#include "ap.h"
+#include "bm.h"
+#include "bmbattle.h"
+#include "bmcontainer.h"
+#include "bmudisp.h"
+#include "bmunit.h"
+#include "bmusemind.h"
+#include "cgtext.h"
+#include "chapterdata.h"
+#include "classchg.h"
+#include "ctc.h"
+#include "fontgrp.h"
+#include "hardware.h"
+#include "prepscreen.h"
+#include "soundwrapper.h"
+#include "statscreen.h"
+#include "sysutil.h"
+#include "uiutils.h"
+#include "worldmap.h"
+#include "constants/chapters.h"
+#include "constants/songs.h"
+
+
+
+
+int GetPrepMenuItemAmt(void)
+{
+    struct ProcPrepMenu * proc = Proc_Find(ProcScr_PrepMenu);
+
+    if (proc != NULL)
+        return proc->max_index;
+    else
+        return 0;
+}
