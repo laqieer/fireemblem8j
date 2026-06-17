4a8d331e4766f8af4d386f8e0708c6d26b001f10
laqieer <laqieer@126.com>
2026-06-17 19:32:34 +0000

    feat(handdecomp): coddog-carve efxFenrirOBJ_Loop

---
+++ src/efxFenrirOBJ_Loop.c
@@ -0,0 +1,25 @@
+#include "global.h"
+#include "anime.h"
+#include "ekrbattle.h"
+#include "efxbattle.h"
+#include "efxmagic.h"
+#include "hardware.h"
+#include "bmlib.h"
+#include "ctc.h"
+
+
+
+
+void efxFenrirOBJ_Loop(struct ProcEfxOBJ * proc)
+{
+    proc->timer++;
+
+    if (proc->timer > proc->terminator)
+    {
+        gEfxBgSemaphore--;
+        AnimDelete(proc->anim2);
+        Proc_Break(proc);
+    }
+
+    return;
+}
