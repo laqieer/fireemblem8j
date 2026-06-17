cffef96a012e4046bc831515c22d069d04cf293b
laqieer <laqieer@126.com>
2026-06-17 19:33:43 +0000

    feat(handdecomp): coddog-carve StartSpellAnimHeal

---
+++ src/StartSpellAnimHeal.c
@@ -0,0 +1,30 @@
+#include "global.h"
+#include "anime.h"
+#include "ekrbattle.h"
+#include "efxbattle.h"
+#include "efxmagic.h"
+#include "hardware.h"
+#include "bmlib.h"
+#include "mu.h"
+#include "bmudisp.h"
+#include "bmmap.h"
+#include "ctc.h"
+
+
+
+extern struct ProcCmd  ProcScr_efxLive[];
+
+void StartSpellAnimHeal(struct Anim * anim)
+{
+    struct ProcEfx * proc;
+
+    SpellFx_Begin();
+    NewEfxSpellCast();
+    SpellFx_ClearBG1Position();
+
+    proc = Proc_Start(ProcScr_efxLive, PROC_TREE_3);
+    proc->anim = anim;
+    proc->timer = 0;
+
+    return;
+}
