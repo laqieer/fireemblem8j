2aef757b9111115faa347ad9060b2cbf1b132d41
laqieer <laqieer@126.com>
2026-06-17 22:26:11 +0000

    feat(handdecomp): carve GameControl_PostChapterSwitch (FAR 2body const-fix: JP Proc_Goto labels 3,15)

---
+++ src/GameControl_PostChapterSwitch.c
@@ -0,0 +1,44 @@
+#include "global.h"
+#include "hardware.h"
+#include "m4a.h"
+#include "soundwrapper.h"
+#include "bmio.h"
+#include "bmunit.h"
+#include "mu.h"
+#include "rng.h"
+#include "event.h"
+#include "opinfo.h"
+#include "bm.h"
+#include "bmsave.h"
+#include "ending_details.h"
+#include "bmlib.h"
+#include "eventinfo.h"
+#include "worldmap.h"
+#include "savemenu.h"
+#include "gamecontrol.h"
+#include "sio.h"
+#include "constants/chapters.h"
+#include "constants/event-flags.h"
+#include "constants/songs.h"
+
+void GameControl_PostChapterSwitch(struct GameCtrlProc* proc) {
+
+    EndAllMus();
+
+    switch (proc->nextAction) {
+    case GAME_ACTION_EVENT_RETURN:
+        Proc_Goto(proc, 3) /* JP label-1 */;
+        break;
+
+    case GAME_ACTION_PLAYED_THROUGH:
+        Proc_Goto(proc, 15) /* JP label-1 */;
+        break;
+
+    case GAME_ACTION_CLASS_REEL:
+    case GAME_ACTION_USR_SKIPPED:
+    default:
+        break;
+    }
+
+    return;
+}
