cd133be33658781eede23c7515a2eb6e45310467
laqieer <laqieer@126.com>
2026-06-17 14:58:45 +0000

    feat(handdecomp): coddog-carve Event15_BgmVolume (auto straight)

---
+++ src/Event15_BgmVolume.c
@@ -0,0 +1,95 @@
+#include "global.h"
+#include "proc.h"
+#include "rng.h"
+#include "hardware.h"
+#include "m4a.h"
+#include "soundwrapper.h"
+#include "fontgrp.h"
+#include "bmio.h"
+#include "bmunit.h"
+#include "bmmap.h"
+#include "bmitem.h"
+#include "bmtrick.h"
+#include "bmidoten.h"
+#include "mu.h"
+#include "chapterdata.h"
+#include "face.h"
+#include "scene.h"
+#include "bm.h"
+#include "bmlib.h"
+#include "bmudisp.h"
+#include "gamecontrol.h"
+#include "uimenu.h"
+#include "prepscreen.h"
+#include "playerphase.h"
+#include "bmbattle.h"
+#include "popup.h"
+#include "muctrl.h"
+#include "mapanim.h"
+#include "helpbox.h"
+#include "worldmap.h"
+#include "cgtext.h"
+#include "bmmind.h"
+#include "eventinfo.h"
+#include "event.h"
+#include "eventscript.h"
+#include "EAstdlib.h"
+#include "constants/backgrounds.h"
+#include "eventcall.h"
+#include "bmdifficulty.h"
+#include "bmfx.h"
+#include "colorfade.h"
+#include "constants/songs.h"
+
+
+struct MapCursorProc
+{
+    /* 00 */ PROC_HEADER;
+    /* 29 */ STRUCT_PAD(0x29, 0x64);
+
+    /* 64 */ s16 x;
+    /* 66 */ s16 y;
+    /* 68 */ u16 subcmd;
+    /* 6A */ u16 timer;
+};
+struct ProcEventPromote
+{
+    /* 00 */ PROC_HEADER;
+
+    /* 29 */ STRUCT_PAD(0x29, 0x58);
+
+    /* 58 */ struct EventEngineProc * event_engine;
+
+    /* 5C */ STRUCT_PAD(0x5c, 0x64);
+
+    /* 64 */ s16 lock;
+};
+
+u8 Event15_BgmVolume(struct EventEngineProc * proc)
+{
+    u8 subcode = EVT_SUB_CMD(proc->pEventCurrent);
+
+    switch (subcode) {
+    case 0:
+        if (EVENT_IS_SKIPPING(proc))
+            return EVC_ADVANCE_CONTINUE;
+
+        StartBgmVolumeChange(0x100, 0x90, 10, proc);
+
+        break;
+
+    case 1:
+        if (EVENT_IS_SKIPPING(proc))
+        {
+            Sound_SetSEVolume(0x100);
+            return EVC_ADVANCE_CONTINUE;
+        }
+
+        StartBgmVolumeChange(0x90, 0x100, 10, proc);
+
+        break;
+
+    } // switch (subcode)
+
+    return EVC_ADVANCE_YIELD;
+}
