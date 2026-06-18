a1f2106e201681c0d3003de8fc2f5007d12b9d8b
laqieer <laqieer@126.com>
2026-06-18 16:01:55 +0000

    feat(handdecomp): PutImg_PrepPopupWindow + MapAnimCallSpellAssocLatonafx (aliased-fn carves; Img/ProcScr binds)

---
+++ src/MapAnimCallSpellAssocLatonafx.c
@@ -0,0 +1,29 @@
+#include "global.h"
+#include "proc.h"
+#include "bmlib.h"
+#include "bmio.h"
+#include "bmunit.h"
+#include "bmbattle.h"
+#include "mapanim.h"
+#include "hardware.h"
+#include "bmlib.h"
+#include "bmitem.h"
+#include "bmmind.h"
+#include "soundwrapper.h"
+#include "ctc.h"
+#include "ap.h"
+#include "eventinfo.h"
+#include "efxbattle.h"
+#include "constants/items.h"
+#include "constants/video-global.h"
+#include "constants/songs.h"
+
+void MapAnimCallSpellAssocLatonafx(struct Unit * unit)
+{
+    struct MAEffectProc * proc = Proc_Start(ProcScr_MapLatonafx, PROC_TREE_3);
+
+    proc->unit = unit;
+
+    proc->xDisplay = 8 * (1 + 2 * (unit->xPos - (gBmSt.camera.x>>4)));
+    proc->yDisplay = 8 * (1 + 2 * (unit->yPos - (gBmSt.camera.y>>4)));
+}
---
+++ src/PutImg_PrepPopupWindow.c
@@ -0,0 +1,35 @@
+#include "global.h"
+#include "bmunit.h"
+#include "bmitemuse.h"
+#include "hardware.h"
+#include "bmlib.h"
+#include "ctc.h"
+#include "face.h"
+#include "icon.h"
+#include "bmudisp.h"
+#include "fontgrp.h"
+#include "bm.h"
+#include "uiutils.h"
+#include "statscreen.h"
+#include "m4a.h"
+#include "soundwrapper.h"
+#include "bmshop.h"
+#include "bmitem.h"
+#include "bmitemuse.h"
+#include "bmcontainer.h"
+#include "worldmap.h"
+#include "helpbox.h"
+#include "sysutil.h"
+#include "prepscreen.h"
+#include "constants/characters.h"
+#include "constants/items.h"
+#include "constants/msg.h"
+#include "constants/songs.h"
+
+//! FE8U = 0x08098C8C
+void PutImg_PrepPopupWindow(int vram, int pal)
+{
+    Decompress(Img_PrepPopupWindow, OBJ_CHR_ADDR(0x0) + vram);
+    ApplyPalette(gUiFramePaletteD, pal + 0x10);
+    return;
+}
