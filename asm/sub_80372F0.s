50ec8158c4892333e14a5eab34a69565b9c6b575
laqieer <laqieer@126.com>
2026-06-18 03:46:12 +0000

    name: mnemonic-prefix BattleForecast_LoopSlideOut (2-signal)

--- asm/sub_80372F0.s
+++ asm/sub_80372F0.s
@@ -4,11 +4,11 @@
 	.set sub_80011D0, 0x080011D0 + 1
 	.set sub_8002DE4, 0x08002DE4 + 1
 	.section .text.sub_80372F0, "ax", %progbits
-@ sub_80372F0 @ JP 0x080372F0 - region-different, gbadisasm descriptive asm (D23)
+@ BattleForecast_LoopSlideOut @ JP 0x080372F0 - region-different, gbadisasm descriptive asm (D23)
 	.thumb
-	.global sub_80372F0
+	.global BattleForecast_LoopSlideOut
 	.thumb_func
-sub_80372F0:
+BattleForecast_LoopSlideOut:
 	push {r4, r5, r6, r7, lr}
 	mov r7, sl
 	mov r6, sb
