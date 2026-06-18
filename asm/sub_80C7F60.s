d27896298af374a94ca0ca2f17dfe7d295435796
laqieer <laqieer@126.com>
2026-06-18 03:38:18 +0000

    name: mnemonic-prefix GmapAutoMu_WaitInitialDelay (2-signal)

--- asm/sub_80C7F60.s
+++ asm/sub_80C7F60.s
@@ -1,11 +1,11 @@
 	.syntax unified
 	.set sub_8002DE4, 0x08002DE4 + 1
 	.section .text.sub_80C7F60, "ax", %progbits
-@ sub_80C7F60 @ JP 0x080C7F60 - region-different, gbadisasm descriptive asm (D23)
+@ GmapAutoMu_WaitInitialDelay @ JP 0x080C7F60 - region-different, gbadisasm descriptive asm (D23)
 	.thumb
-	.global sub_80C7F60
+	.global GmapAutoMu_WaitInitialDelay
 	.thumb_func
-sub_80C7F60:
+GmapAutoMu_WaitInitialDelay:
 	push {lr}
 	adds r1, r0, #0
 	ldrh r0, [r1, #0x3c]
