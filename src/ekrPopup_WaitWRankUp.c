78b37fdc49beb5de26c8cafec5438ce65abe7d9d
laqieer <laqieer@126.com>
2026-06-19 02:00:24 +0000

    feat(handdecomp): ekrPopup_WaitWpnBroke/WaitWRankUp/WaitWRankUp2 (JP ProcEkrPopup buffer-field reorder via pointer casts; struct-offset vein REACHABLE)

---
+++ src/ekrPopup_WaitWRankUp.c
@@ -0,0 +1,30 @@
+#include "global.h"
+#include "hardware.h"
+#include "bmitem.h"
+#include "fontgrp.h"
+#include "icon.h"
+#include "anime.h"
+#include "ekrbattle.h"
+#include "efxbattle.h"
+#include "ekrpopup.h"
+#include "ekrlevelup.h"
+#include "soundwrapper.h"
+#include "ctc.h"
+#include "constants/songs.h"
+
+
+
+
+void ekrPopup_WaitWRankUp(struct ProcEkrPopup *proc)
+{
+    if ((*(int *)((char *)proc + 0x4c)) == 0) {
+        Proc_Break(proc);
+        return;
+    }
+    
+    if (++proc->timer > proc->terminator) {
+        AnimDelete(proc->anim);
+        SpellFx_ClearBG1();
+        Proc_Break(proc);
+    }
+}
---
+++ src/ekrPopup_WaitWRankUp2.c
@@ -0,0 +1,30 @@
+#include "global.h"
+#include "hardware.h"
+#include "bmitem.h"
+#include "fontgrp.h"
+#include "icon.h"
+#include "anime.h"
+#include "ekrbattle.h"
+#include "efxbattle.h"
+#include "ekrpopup.h"
+#include "ekrlevelup.h"
+#include "soundwrapper.h"
+#include "ctc.h"
+#include "constants/songs.h"
+
+
+
+
+void ekrPopup_WaitWRankUp2(struct ProcEkrPopup *proc)
+{
+    if ((*(int *)((char *)proc + 0x48)) == 0) {
+        Proc_Break(proc);
+        return;
+    }
+
+    if (++proc->timer > proc->terminator) {
+        AnimDelete(proc->anim);
+        SpellFx_ClearBG1();
+        Proc_Break(proc);
+    }
+}
