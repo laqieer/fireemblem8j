a376b5f8c04089653b102715e5ecc5b677804af9
laqieer <laqieer@126.com>
2026-06-20 08:40:39 +0000

    feat(handdecomp): carve PointsNumberMover_Init (matching C + named)
    DECL_ONLY: extern gSioPoints_3 (const u8[]) + gSioPoints_5 (const int[]).
    Region-same Sio points number-mover init (redundant if/else branch
    preserved as in fe8u). matching-C 7674 -> 7675.

---
+++ src/sub_80498B8.c
@@ -0,0 +1,46 @@
+#include "global.h"
+
+#include "hardware.h"
+#include "bmunit.h"
+#include "fontgrp.h"
+#include "sio.h"
+#include "sio_core.h"
+
+extern const u8 gSioPoints_3[];
+extern const int gSioPoints_5[];
+
+void PointsNumberMover_Init(struct PointsNumberMoverProc * proc)
+{
+    struct Unit * unit = GetUnit(proc->unitId);
+
+    int idx = gSioPoints_1[gSioSt->selfId][proc->playerId];
+
+    if (proc->unk_40 != 0)
+    {
+        if (unit->xPos == 8) // redundant?
+        {
+            proc->x = unit->xPos * 16 - 16;
+        }
+        else
+        {
+            proc->x = unit->xPos * 16 - 16;
+        }
+
+        proc->y = unit->yPos * 16;
+    }
+    else
+    {
+        proc->x = unit->xPos * 16 + gSioPoints_5[idx * 2 + 0] - 12;
+        proc->y = unit->yPos * 16 + gSioPoints_5[idx * 2 + 1];
+    }
+
+    proc->xTarget = gSioPoints_3[idx * 2 + 0] * 8 + 8;
+    proc->yTarget = gSioPoints_3[idx * 2 + 1] * 8 + 8;
+
+    SetTextFont(&Font_Sio_0);
+    SioDrawNumber(&gUnk_Sio_1[0], proc->playerId * 32 + 24, TEXT_COLOR_SYSTEM_BLUE, proc->difference);
+
+    proc->timer = 0;
+
+    return;
+}
